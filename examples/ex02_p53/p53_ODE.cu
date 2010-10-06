a1d68e95f4c7eac09036596db877202921461fa1
finalstate <finalstate@07fad6ea-e870-49fc-a7a4-2cbfd2015da0>
2010-10-06 16:30:44 +0000

    Reverted to old parser

--- examples/ex02_p53/p53_ODE.cu
+++ examples/ex02_p53/p53_ODE.cu
@@ -9,9 +9,9 @@ struct myFex{
 
 
 
-        ydot[0]=((tex2D(param_tex,4,tid)*tex2D(param_tex,8,tid))-(tex2D(param_tex,5,tid)*y[0])-(tex2D(param_tex,6,tid)*y[0]*y[2]/(y[0]+tex2D(param_tex,7,tid)*tex2D(param_tex,8,tid))))/tex2D(param_tex,0,tid);
-        ydot[1]=((tex2D(param_tex,1,tid)*y[0])-(tex2D(param_tex,2,tid)*y[1]))/tex2D(param_tex,0,tid);
-        ydot[2]=((tex2D(param_tex,2,tid)*y[1])-(tex2D(param_tex,3,tid)*y[2]))/tex2D(param_tex,0,tid);
+        ydot[0]=(1.0*(tex2D(param_tex,4,tid)*tex2D(param_tex,8,tid))-1.0*(tex2D(param_tex,5,tid)*y[0])-1.0*(tex2D(param_tex,6,tid)*y[0]*y[2]/(y[0]+tex2D(param_tex,7,tid)*tex2D(param_tex,8,tid))))/tex2D(param_tex,0,tid);
+        ydot[1]=(1.0*(tex2D(param_tex,1,tid)*y[0])-1.0*(tex2D(param_tex,2,tid)*y[1]))/tex2D(param_tex,0,tid);
+        ydot[2]=(1.0*(tex2D(param_tex,2,tid)*y[1])-1.0*(tex2D(param_tex,3,tid)*y[2]))/tex2D(param_tex,0,tid);
 
     }
 };

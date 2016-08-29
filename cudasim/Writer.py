2b836c800e9e83cf93f17020cb517b36ccb014be
James Scott-Brown <james@jamesscottbrown.com>
2016-08-29 12:42:36 +0100

    add ParsedModel.py and Writer.py classes (copied directly form abc-sysbio)

---
+++ cudasim/Writer.py
@@ -0,0 +1,11 @@
+from ParsedModel import ParsedModel
+
+
+class Writer:
+    def __init__(self, sbmlFileName, modelName="", inputPath="", outputPath=""):
+        self.parsedModel = ParsedModel()
+
+        if modelName == "":
+            self.parsedModel.name = "unnamedModel"
+        else:
+            self.parsedModel.name = modelName
